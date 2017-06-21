93dbec5d40bdfaadce515db406312c735f48f725
Rahul <rahulpatil2387@gmail.com>
2017-06-22 03:57:26 +0530

    Update product new create action and views

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,8 +9,27 @@ class ProductsController < ApplicationController
   end
 
   def new
+    @product = Product.new()
+    @product.type = params[:type]
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    respond_to do |format|
+      if @product.save
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
+      else
+        format.html { render action: "new" }
+      end
+    end
   end
 
   def edit
+  end
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :color, :status, :inward_date, :type, :description)
   end
 end
