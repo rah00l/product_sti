579b4b3893d40417f3de0b248d60da2cf8e01832
Rahul <rahulpatil2387@gmail.com>
2017-06-22 05:08:13 +0530

    Need to write test cases for these files

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -16,11 +16,11 @@ RSpec.describe ProductsController, type: :controller do
     end
   end
 
-  describe "GET #edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
+  # describe "GET #edit" do
+  #   it "returns http success" do
+  #     get :edit
+  #     expect(response).to have_http_status(:success)
+  #   end
+  # end
 
 end
