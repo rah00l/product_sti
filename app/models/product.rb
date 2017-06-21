eff44189fb21cabe3c4a41707c660c9007a21864
Rahul <rahulpatil2387@gmail.com>
2017-06-22 04:15:13 +0530

    Add a class method to get sum of prices of given product type

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,9 @@ class Product < ApplicationRecord
 	# Validation
 	validates :name, :price, :inward_date, :type, :description, presence: true
 	validates :status, inclusion: { in: [ true, false ] }
+
+	def self.price_sum(product_type:)
+		where(type: product_type).
+		sum(:price)
+	end
 end
