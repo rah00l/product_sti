50a7ea1dddc4a94bd121c2ec67b7c0700348742d
Rahul <rahulpatil2387@gmail.com>
2017-06-22 03:52:20 +0530

    Update route to have correct entity path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
   resources :products
+  resources :pens, controller: "products", type: "Pen"
+  resources :notebooks, controller: "products", type: "Notebook"
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
